b9f179e3730b8e57ca6bce4ab39f5d4566cf2025
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-03 19:04:28 -0400

    Enhance Product class and introduce UnitType enum
    Expanded the `Product` class with new properties and methods, including functionality for managing stock levels and logging. Added private fields for product details and methods for using and increasing stock, as well as updating low stock status.
    
    Introduced the `UnitType` enum in `UnitType.cs` to categorize measurement units for products, with values for `PerItem`, `PerBox`, and `PerKg`. Added necessary `using` directives for improved functionality.

--- BethanysPieShop.InventoryManagement/BethanysPieShop.InventoryManagement/Product.cs
+++ BethanysPieShop.InventoryManagement/BethanysPieShop.InventoryManagement/Product.cs
@@ -8,5 +8,52 @@ namespace BethanysPieShop.InventoryManagement
 {
     public class Product
     {
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        private int maxItemsStocked = 0;
+
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockThreshold = false;
+
+        public void UseProduct(int items)
+        {
+            if (items <= amountInStock)
+            {
+                //use the items
+                amountInStock -= items;
+
+                UpdateLowStock();
+
+                Log($"Amount in stock updated. Now {amountInStock} items in stock");
+            }
+            else
+            {
+                Log($"Not enough items in stock for {CreateSimpleProductRepresentation()} {amountInStock} " +
+                    $"available but {items} requested.");
+            }
+        }
+
+        public void IncreaseStock()
+        {
+            amountInStock++;
+        }
+
+        private void UpdateLowStock()
+        {
+            if (amountInStock < 10)
+            {
+                isBelowStockThreshold = true;
+            }
+        }
+
+        private void Log(string message)
+        {
+            //can be written to a file
+            Console.WriteLine(message);
+        }
+
     }
 }
---
+++ BethanysPieShop.InventoryManagement/BethanysPieShop.InventoryManagement/UnitType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.InventoryManagement
+{
+    public enum UnitType //each value responds with int value
+    {
+        PerItem,
+        PerBox,
+        PerKg
+    }
+}
