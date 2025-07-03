ac8c2e953145c441a740b6d750c40e9df01a5b34
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-03 12:46:07 -0400

    Add Car and Employee classes with encapsulation
    Introduces a new `Car` class demonstrating encapsulation
    with private fields and public properties. Adds an `Employee`
    class as an example of basic OOP, featuring private fields,
    a public property, a constructor, and a placeholder method
    for future implementation.

---
+++ oop-pluralsight-2/oop-pluralsight-2/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_pluralsight_2
+{
+    //Example of Encapsulation
+    internal class Car
+    {
+        private int id; //usable from class itself
+        private int temperature; //important data hidden so can't be changed
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (value > 0)
+                    id = value;
+            }
+        }
+       
+
+    }
+}
---
+++ oop-pluralsight-2/oop-pluralsight-2/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_pluralsight_2
+{
+    //example of basic oop
+    public class Employee
+    {
+        public Employee() //class
+        {
+
+        }
+
+        //fields - variables defined from the class level to instantiate employee objects (usually private)
+        private string firstName;
+        private int age; 
+
+        //Properties
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public void PerformWork()
+        {
+            //method implementation goes here
+        }
+    }
+}
