2136aa60768a264a4319c7f652fe2831e3188d0d
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-03 13:16:22 -0400

    polymorphism example

---
+++ oop-pluralsight-2/oop-pluralsight-2/CarPoly.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_pluralsight_2
+{
+    public class CarPoly
+    {
+        public virtual void Drive()
+        {
+
+        }
+    }
+
+    public class ElectricCar : CarPoly
+    {
+        public override void Drive()
+        {
+            base.Drive();
+        }
+    }
+}
